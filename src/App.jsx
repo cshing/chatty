d2d30f4455586498db2d05efdad5fa9453bc2016
Carmen Shing <carmenshing@gmail.com>
2018-07-11 18:14:15 +0000

    installed ws and connected App to websocket

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,9 @@ export default class App extends Component {
       // Calling setState will trigger a call to render() in App and all child components.
       this.setState({messages: messages})
     }, 3000);
+
+    this.socket = new WebSocket('ws://localhost:3001'); //${window.location.host}
+    console.log('Connected to server')
   }
 
   render () {
