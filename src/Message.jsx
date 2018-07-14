fd8a33457809b7e0e80aa7712c33f1f6294b6f90
Carmen Shing <carmenshing@gmail.com>
2018-07-14 20:40:23 +0000

    undo changes to fix bugs

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
 
 export default function Message(props) {
-
-    const imgEnding = props.content.substr(props.content.length - 3);
-    if ( imgEnding === 'jpg' || imgEnding === 'peg' || imgEnding === 'png' || imgEnding === 'gif' ) {
-        return (
-            <div className="message">
-                <span className="message-notification"> { props.notification } </span>
-                <span className="message-username" style={{color:props.userColor}} > { props.username } </span>
-                <span className="message-content"> <img className="content-image" src={`${props.content}`} alt="imageUrl"/> </span>
-            </div>
-        );
-
-    } else {
-        return (
-            <div className="message">
-                <span className="message-notification"> { props.notification } </span>
-                <span className="message-username" style={{color:props.userColor}} > { props.username } </span>
-                <span className="message-content"> { props.content } </span>
-            </div>
-        );
-    }
+    return (
+        <div className="message">
+            <span className="message-notification"> { props.notification } </span>
+            <span className="message-username" style={{color:props.userColor}} > { props.username } </span>
+            <span className="message-content"> { props.content } </span>
+        </div>
+    );
 }
 
